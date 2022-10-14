4acdfc155e05cd2823844c0dcefc82ccf760bd0e
Code Sandbox <hello@codesandbox.io>
2022-10-14 15:39:34 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e3a64fbff876b331373006824d4cc4fd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c17c3b0cd632207fb6b3682b6a3dbf",
+    "url": "https://rastogi26.github.io/csb-nnu8ov/index.html"
+  },
+  {
+    "revision": "10ec986d898ce9e91827",
+    "url": "https://rastogi26.github.io/csb-nnu8ov/static/js/2.0e3b9301.chunk.js"
+  },
+  {
+    "revision": "88bd2c5c397eff501c1c",
+    "url": "https://rastogi26.github.io/csb-nnu8ov/static/js/main.fd9f5b64.chunk.js"
+  },
+  {
+    "revision": "4b7ca0e4a7e6db8e83b7",
+    "url": "https://rastogi26.github.io/csb-nnu8ov/static/js/runtime-main.2921eead.js"
+  }
+]);
